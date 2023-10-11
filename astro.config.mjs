35d3f4ec2461a83055b01a18050182c90de049f6
Tadas Petra <tadas@tadaspetra.com>
2023-10-11 08:40:57 -0500

    Updated to astro 3.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  output: 'server',
   markdown: {
     // Example: Switch to use prism for syntax highlighting in Markdown
-    syntaxHighlight: 'shiki',
+    syntaxHighlight: 'shiki'
   },
   vercel: {
-    analytics: true,
-  }
+    analytics: true
+  },
+  adapter: vercel()
 });

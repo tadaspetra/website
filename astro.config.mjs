e4ef35ee24406754fea06936832c23d919a9a821
Tadas Petra <tadas@tadaspetra.com>
2023-10-11 08:33:54 -0500

    I think I need this too

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  output: 'server',
   markdown: {
     // Example: Switch to use prism for syntax highlighting in Markdown
     syntaxHighlight: 'shiki',
